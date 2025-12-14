a44c2c39e40be9898bee75d765bd227e70f9cda8
Seena Mohajeran <seenamohajeran@gmail.com>
2025-12-14 14:55:49 -0800

    upload

--- app/main.py
+++ app/main.py
@@ -5,8 +5,11 @@ app = FastAPI(title="Phase 1 ML Serving API")
 
 # Include API router
 app.include_router(router, prefix="/api")
+#app = FastAPI(debug=True)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# uvicorn app.main:app --reload
 
